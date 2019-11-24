04c35641c3d62efcd4ba5871b10295833e15be46
Chase Philips <chasehomedecor@gmail.com>
2019-11-24 17:24:05 -0500

    make table much better

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -3,11 +3,14 @@ import Grid from "@material-ui/core/Grid";
 import MUIDataTable from "mui-datatables";
 import { useQuery } from "@apollo/react-hooks";
 import AreasLoading from "../areas/AreasLoading";
+import AreasLoadError from "../areas/AreasLoadError";
 import COMPLETED_DUTIES_QUERY from "../graphql/queries/completedDutiesQuery";
 import prune from "../prune";
+import dayjs from "dayjs";
 
 const Admin = () => {
-  const { loading, error, data } = useQuery(COMPLETED_DUTIES_QUERY);
+  const { error, data } = useQuery(COMPLETED_DUTIES_QUERY);
+  if (error) return <AreasLoadError />;
   if (!data) return <AreasLoading />;
   const columns = [
     {
@@ -20,16 +23,30 @@ const Admin = () => {
     },
     {
       name: "lastCompletedBy",
-      label: "Marked Completed By"
+      label: "Completed By"
+    },
+    {
+      name: "createdAt",
+      label: "Completion Date",
+      options: {
+        customBodyRender: (value, tableMeta, updateValue) => (
+          <DateDisplay value={value} />
+        )
+      }
     },
     {
       name: "expiresAt",
-      label: "Renewal Point"
+      label: "Renewal Point",
+      options: {
+        customBodyRender: (value, tableMeta, updateValue) => (
+          <DateDisplay value={value} />
+        )
+      }
     }
   ];
 
   const completedDuties = prune(data.completedDuties);
-  console.log(completedDuties);
+  // console.log(completedDuties);
   const options = {
     filterType: "checkbox"
   };
@@ -48,3 +65,7 @@ const Admin = () => {
 };
 
 export default Admin;
+
+const DateDisplay = ({ value, showTime = false }) => (
+  <div>{dayjs(value).format(`${showTime ? "HH:mm" : ""} MMM D YYYY`)}</div>
+);
